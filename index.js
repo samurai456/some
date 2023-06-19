fb64c676623f104801e6c1fb3c3acfbee48c5936
samurai456 <ula.sadikov2001@gmail.com>
2023-06-19 23:14:45 +0500

    's'

--- index.js
+++ index.js
@@ -1,22 +1,40 @@
 const express = require('express');
 const session = require('express-session');
+const cors = require('cors');
 const app = express();
 
 app.use(express.json());
-app.use((req, res, next)=>{
-    res.setHeader('Access-Control-Allow-Origin', req.get('origin'));
-    res.setHeader('Access-Control-Allow-Methods', ['GET', 'POST', 'PUT', 'DELETE']);
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader('Access-Control-Allow-Headers', "Content-Type");
-    next();
-});
-app.use(session({secret:'some key', cookie: {sameSite: false} }));
+
+app.use(
+    session({
+        name: "SESS_NAME",
+        secret: "SESS_SECRET",
+        saveUninitialized: false,
+        resave: false,
+        cookie: {
+            sameSite: false,
+            maxAge: 10000,
+            httpOnly: true,
+        },
+    })
+);
+
+app.use(
+    cors({
+        origin: 'https://some-front-test-9098.onrender.com/',
+        methods: ["POST", "PUT", "GET", "DELETE"],
+        credentials: true,
+    })
+);
 
 app.post('/', (req, res)=>{
-    
-    let r = req.session.user;
-    req.session.user = {id: 'someUser', password: 'password'};
-    res.json(JSON.stringify(r));
+    console.log('request');
+    if(req.session.user){
+        res.json(JSON.stringify(req.session.user));
+        return
+    }
+    req.session.user = {id: 'someUser', password: 'password11'};
+    res.json(JSON.stringify({type: 'making session'}))
 });
 
 app.listen(8000, ()=>console.log('good...'));
